04121bf8b3a0eefca86d6352a358dbc2caf85267
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-02-12 18:54:32 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+  int x;
+  cout<<"Enter a number:";
+  cin>>x;
+
+  int s=x*x;
+  cout<<"Square of "<<x<<" is "<<s;
+  getch();
+  return 0;
+}
