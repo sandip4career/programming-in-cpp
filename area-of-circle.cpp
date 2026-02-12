f0fe97851030324bc58b1413fb347c741a8e8011
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-02-12 18:00:36 +0530

    Add files via upload

---
+++ area-of-circle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int r;
+    float a;
+    cout<<"Enter a radius of circle:";
+    cin>>r;
+    a= 3.14* r*r;
+
+    cout<<"Area of circle is "<<a;
+    return 0;
+}
