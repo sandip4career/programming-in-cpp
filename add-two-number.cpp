294601b8c4d9c04b96186aadcf7bd15fcb17ddec
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-06-18 21:49:59 +0530

    Add files via upload

---
+++ add-two-number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void add(int,int);
+
+int main()
+{
+    add(45,65);
+}
+
+void add(int a,int b)
+    {
+
+    cout<<"Enter two number:";
+    cin>>a>>b;
+
+
+    cout<<"Addition of two number:"<<a+b;
+
+    }
+
