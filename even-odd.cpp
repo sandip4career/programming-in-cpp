7041aced76db1f4bd3da4679427c0ef1188aed3d
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-06-23 14:49:34 +0530

    Add files via upload

---
+++ even-odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+
+    cout<<"Enter a Number:";
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<num<<" is even";
+    }else{
+        cout<<num<<" is odd";
+    }
+    return 0;
+}
