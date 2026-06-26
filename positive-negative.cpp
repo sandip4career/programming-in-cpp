e8f6c0df4e92b9c2f749957e6292ee100a2bdd9d
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-06-26 13:50:04 +0530

    Add files via upload

---
+++ positive-negative.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+
+    if(num>0){
+        cout<<num<<" is positive";
+    }else{
+        if(num==0){
+            cout<<"enter other than 0";
+
+        }else
+        cout<<num<<" is negative";
+    }
+    return 0;
+}
