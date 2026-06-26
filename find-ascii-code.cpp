3d86c86140dcc0f2ab4c03cc1d7f3f731068f1f6
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-06-26 14:21:48 +0530

    Add files via upload

---
+++ find-ascii-code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+
+    cout<<"Enter a character:";
+    cin>>ch;
+
+    cout<<"ascii code of "<<ch<<":"<<int(ch);
+    return 0;
+}
